ad224ec057dbe0c3342d2a8089bb4cd07dfe1369
Gaston Fernandez <gastonfernandez@Gastons-MacBook-Pro.local>
2019-05-17 16:54:23 -0300

    api url

--- src/config/Constants.js
+++ src/config/Constants.js
@@ -1,5 +1,5 @@
 
-const API_URL = process.env.API_URL || "http://localhost:8080/mock-server"
+const API_URL = "https://integrations.dev.fintechpeople.io:4443/mock-server/" //"http://localhost:8080/mock-server"
 
 module.exports={
     API_URL
